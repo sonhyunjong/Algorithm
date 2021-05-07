e44efa6b8fa228b4bdf050dd013062be10f745e7
sonhyunjong <37691789+sonhyunjong@users.noreply.github.com>
2021-05-07 10:02:59 +0900

    test

---
+++ BAEKJOON/src/Test/Test.java
@@ -0,0 +1,17 @@
+package Test;
+
+import java.util.Scanner;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		String name=sc.nextLine();
+		String sr=sc.next();
+		
+		System.out.println(name);
+		System.out.println(sr);
+	}
+
+}
