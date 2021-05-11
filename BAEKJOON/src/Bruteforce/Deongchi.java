2c0ebbc0e39136dd1c97a4dbeee158ee493e0e34
sonhyunjong <37691789+sonhyunjong@users.noreply.github.com>
2021-05-12 01:25:59 +0900

    BufferedReader 사용

--- BAEKJOON/src/Bruteforce/Deongchi.java
+++ BAEKJOON/src/Bruteforce/Deongchi.java
@@ -1,24 +1,27 @@
 package Bruteforce;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Deongchi {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 		//입력 : 첫 줄에는 전체 사람의 수 N이 주어진다. 그리고 이어지는 N개의 줄에는 각 사람의 몸무게와 키를 나타내는 양의 정수 x와 y가 하나의 공백을 두고 각각 나타난다.
 		//출력 : 여러분은 입력에 나열된 사람의 덩치 등수를 구해서 그 순서대로 첫 줄에 출력해야 한다. 단, 각 덩치 등수는 공백문자로 분리되어야 한다.
 		
-		Scanner scanner = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
-		int count = scanner.nextInt();
+		int count = Integer.parseInt(br.readLine());
 		
 		int[][] deongchi = new int[count][2];
 		int level[] = new int[count];
+		String[] sp;
 		
 		for(int i = 0; i < count; i++) {
-			for(int j = 0; j < deongchi[i].length; j++) {
-				deongchi[i][j] = scanner.nextInt();
-			}
+			sp = br.readLine().split(" ");			
+			deongchi[i][0] = Integer.parseInt(sp[0]);	 
+			deongchi[i][1] = Integer.parseInt(sp[1]);	
 		}
 		
 		//덩치 등수 매기기
